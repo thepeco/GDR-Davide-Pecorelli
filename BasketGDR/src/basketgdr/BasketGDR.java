319cd0e4a7337e5836cbf69b7d9fe4cbd6f4d81d
thepeco <pecorelli.davide@franchettisalviani.net>
2026-03-16 09:26:00 +0100

    Nuova interfaccia "jFrame form"

--- BasketGDR/src/basketgdr/BasketGDR.java
+++ BasketGDR/src/basketgdr/BasketGDR.java
@@ -14,7 +14,13 @@ public class BasketGDR {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            // Crea l'istanza della finestra e la rende visibile
+            new Interfaccia().setVisible(true);
+        }
+    });
     }
     
 }
