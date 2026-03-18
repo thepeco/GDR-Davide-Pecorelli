1336e8a28687eb8e35cf477c74c4949601ac1c0f
thepeco <pecorelli.davide@franchettisalviani.net>
2026-03-18 09:26:48 +0100

    Creazione classe Nemico (Base)

--- BasketGDR/src/basketgdr/Nemico.java
+++ BasketGDR/src/basketgdr/Nemico.java
@@ -3,12 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package basketgdr;
-
+ import java.util.ArrayList;
 /**
  *
  * @author pecorelli.davide
  */
 public class Nemico {
     
+    private int vitaNemico;
+    ArrayList <String> listaNemici = new ArrayList();
+    private int dannoBase;
     
+    public Nemico (int vitaNemico,int dannoBase){
+        
+        this.vitaNemico = vitaNemico;
+        this.dannoBase = dannoBase;
+    }
+    
+    public void setVita(int vitaNemico){
+        
+        
+    }
+    
+    public void reazioneNemico (Personaggio p){
+        
+    }
 }
